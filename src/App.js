bf56c87fb69784c3ecb252bdd244b87a6f7364e5
Anas Shams <anasshams1960@gmail.com>
2024-05-11 00:11:54 +0530

    edited 2.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './login';
-import TaskPage from './TaskPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import TaskPage from './components/TaskPage';
+import Login from './components/login';
+
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/tasks/:taskType" component={TaskPage} />
-      </Switch>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Login/>} />
+          <Route path="/tasks/:taskType"  element={<TaskPage/>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
-export default App
+export default App
